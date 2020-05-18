cbce7cbbf19d775ecfcb6c6f84d0d7983901f9bc
Andre Freeman <asf@mail.usf.edu>
2020-05-18 19:54:25 -0400

    Made Put request in ColorList

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import axiosWithAuth from "../util/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -18,9 +18,15 @@ const ColorList = ({ colors, updateColors }) => {
 
   const saveEdit = e => {
     e.preventDefault();
-    // Make a put request to save your updated color
-    // think about where will you get the id from...
-    // where is is saved right now?
+
+    axiosWithAuth()
+    .put(`/colors/${colorToEdit.id}`, colorToEdit)
+    .then(res =>{
+      const filterColors = colors.filter(item => item.id !== colorToEdit.id)
+      updateColors([...filterColors, colorToEdit]);
+      setEditing(false);
+    })
+    .catch(err => console.log("ERROR", err))
   };
 
   const deleteColor = color => {
