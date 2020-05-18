5cf33e190652dab5b16edb161736abe60915db85
Andre Freeman <asf@mail.usf.edu>
2020-05-18 19:33:16 -0400

    Added API get to BubblePage.js

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import {axiosWithAuth} from "../util/axiosWithAuth";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
 const BubblePage = () => {
-  const [colorList, setColorList] = useState([]);
-  // fetch your colors data from the server when the component mounts
-  // set that data to the colorList state property
+    const [colorList, setColorList] = useState([]);
+    useEffect(() => {
+      axiosWithAuth()
+          .get('/colors')
+          .then(res => setColorList(res.data))
+          .catch(err => console.log(err))
+  }, [colorList]);
 
   return (
     <>
